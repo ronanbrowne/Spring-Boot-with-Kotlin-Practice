4a6d83849eb6085787629b60428dba449433511d
Ronan Browne <ronanbrowne88@gmail.com>
2021-01-17 14:14:43 +0000

    Add Controller and simple Method

---
+++ src/main/kotlin/com/browner/practice/browner/HelloWorldController.kt
@@ -0,0 +1,14 @@
+package com.browner.practice.browner
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String = "Hello, This is a REST endpoint"
+
+}
